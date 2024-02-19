d088bf0d54470efb9baa9e374c76bba2b7e84a88
Paul-louis Gomis <paullouis.gomis.etu@univ-lille.fr>
2024-02-19 10:54:41 +0100

    add initDB

---
+++ initDB.sql
@@ -0,0 +1,8 @@
+drop table if exists ingredients;
+CREATE TABLE ingredients(id int, nom text,prix int);
+INSERT INTO ingredients VALUES (1,'pomme de terre',3);
+INSERT INTO ingredients VALUES (2,'poivrons',2);
+INSERT INTO ingredients VALUES (3,'chorizo',4);
+INSERT INTO ingredients VALUES (4,'lardons',4);
+INSERT INTO ingredients VALUES (5,'aubergines',2);
+INSERT INTO ingredients VALUES (6,'champignons',2);
