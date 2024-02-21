817165e9e502b91fff24094674f098ab03305d70
Mathis Decoster <mathis.decoster.etu@univ-lille.fr>
2024-02-21 10:26:18 +0100

    PizzaRestApi à jour

---
+++ WEB-INF/src/controleurs/PizzaRestApi.java
@@ -0,0 +1,103 @@
+package controleurs;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import dao.IngredientDAO;
+import dao.PizzaDAO;
+import dto.Ingredients;
+import dto.Pizzas;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.WebServlet;
+
+@WebServlet("/pizzas/*")
+public class PizzaRestApi extends HttpServlet {
+    PizzaDAO dao = new PizzaDAO();
+
+    public void doGet(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, java.io.IOException {
+        res.setContentType("application/json;charset=UTF-8");
+        PrintWriter out = res.getWriter();
+        String info = req.getPathInfo();
+        ObjectMapper objectMapper = new ObjectMapper();
+        if (info == null || info.equals("/")) {
+            List<Pizzas> l = dao.findAll();
+            String jsonstring = objectMapper.writeValueAsString(l);
+            out.print(jsonstring);
+            return;
+        }
+        String[] splits = info.split("/");
+        if (splits.length > 3) {
+            res.sendError(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        String id = splits[1];
+        Pizzas e = dao.findById(Integer.parseInt(id));
+        if (e == null) {
+            res.sendError(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+        if (splits.length == 2) {
+            out.print(objectMapper.writeValueAsString(e));
+        } else if (splits.length == 3 && splits[2].equals("prixfinal")) {
+            int prixfinal = 0;
+            for (Ingredients ing : e.getIngredients()) {
+                prixfinal += ing.getPrix();
+            }
+            prixfinal += e.getPrixBase();
+            out.print(prixfinal);
+        } else {
+            res.sendError(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+        return;
+    }
+
+    public void doPost(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, java.io.IOException {
+        res.setContentType("application/json;charset=UTF-8");
+        PrintWriter out = res.getWriter();
+        String info = req.getPathInfo();
+        if (info == null || info.equals("/")) {
+            ObjectMapper objectMapper = new ObjectMapper();
+            Pizzas e = objectMapper.readValue(req.getReader(), Pizzas.class);
+            if (!dao.save(e)) {
+                res.sendError(HttpServletResponse.SC_CONFLICT);
+                return;
+            }
+            out.print(objectMapper.writeValueAsString(e));
+        } else {
+            res.sendError(HttpServletResponse.SC_NO_CONTENT);
+            return;
+        }
+    }
+
+    protected void doDelete(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        res.setContentType("application/json;charset=UTF-8");
+        PrintWriter out = res.getWriter();
+        String info = req.getPathInfo();
+        ObjectMapper objectMapper = new ObjectMapper();
+        String[] splits = info.split("/");
+        int id = Integer.parseInt(splits[1]);
+        if(splits.length == 2){
+            if (!dao.delete(id)) {
+                res.sendError(HttpServletResponse.SC_CONFLICT);
+                return;
+            }
+            List<Pizzas> l = dao.findAll();
+            String jsonstring = objectMapper.writeValueAsString(l);
+            out.print(jsonstring);
+        } else if(splits.length == 3){
+            String idIngredient = splits[2];
+            dao.deleteIngredientFromPizza(id, idIngredient);
+            Pizzas e = dao.findById(id);
+            out.print(objectMapper.writeValueAsString(e));
+        }
+        
+    }
+}
