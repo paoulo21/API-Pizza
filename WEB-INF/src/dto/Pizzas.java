a087b36a3049305d8fcea7aeb7455baa0e68fbe8
Paul-louis Gomis <paullouis.gomis.etu@univ-lille.fr>
2024-02-21 10:03:03 +0100

    add pizza dto

---
+++ WEB-INF/src/dto/Pizzas.java
@@ -0,0 +1,66 @@
+package dto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pizzas {
+    private int id;
+    private String nom;
+    private int prixBase;
+    private String pate;
+    private List<Ingredients> ingredients;
+
+    public Pizzas(int id, String nom, int prixBase, String pate) {
+        this.id = id;
+        this.nom = nom;
+        this.prixBase = prixBase;
+        this.pate = pate;
+        ingredients = new ArrayList<>();
+    }
+
+    public Pizzas() {
+    }
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public int getPrixBase() {
+        return prixBase;
+    }
+
+    public String getPate() {
+        return pate;
+    }
+
+    // Setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setPrixBase(int prixBase) {
+        this.prixBase = prixBase;
+    }
+
+    public void setPate(String pate) {
+        this.pate = pate;
+    }
+
+    public List<Ingredients> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(List<Ingredients> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+}
