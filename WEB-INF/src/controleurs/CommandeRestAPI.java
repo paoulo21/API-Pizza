049bb638e00b3733431b0f3655aab7d5cc87afe3
Mathis Decoster <mathis.decoster.etu@univ-lille.fr>
2024-02-22 17:20:11 +0100

    add post non fonctionnel commande

--- WEB-INF/src/controleurs/CommandeRestAPI.java
+++ WEB-INF/src/controleurs/CommandeRestAPI.java
@@ -21,12 +21,51 @@ public class CommandeRestAPI extends HttpServlet{
         res.setContentType("application/json;charset=UTF-8");
         PrintWriter out = res.getWriter();
         String info = req.getPathInfo();
+        String[] splits = info.split("/");
         ObjectMapper objectMapper = new ObjectMapper();
         if (info == null || info.equals("/")) {
             List<Commande> l = dao.findAll();
             String jsonstring = objectMapper.writeValueAsString(l);
             out.print(jsonstring);
             return;
+        }
+
+        if(splits.length == 2){
+            int id = Integer.parseInt(splits[1]);
+            Commande c = dao.findById(id);
+            if (c == null) {
+                res.sendError(HttpServletResponse.SC_NOT_FOUND);
+                return;
+            }
+            out.print(objectMapper.writeValueAsString(c));
+        }
+
+        if(splits.length == 3 && splits[2].equals("prixfinal")){
+            int id = Integer.parseInt(splits[1]);
+            Commande c = dao.findById(id);
+            if (c == null) {
+                res.sendError(HttpServletResponse.SC_NOT_FOUND);
+                return;
+            }
+            out.print(objectMapper.writeValueAsString(c.getPrixTotal()));
+        }
+        
+    }
+
+    public void doPost(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, java.io.IOException {
+        res.setContentType("application/json;charset=UTF-8");
+        PrintWriter out = res.getWriter();
+        String info = req.getPathInfo();
+        String[] splits = info.split("/");
+        if (info == null || info.equals("/")) {
+            ObjectMapper objectMapper = new ObjectMapper();
+            Commande c = objectMapper.readValue(req.getReader(), Commande.class);
+            if (!dao.save(c)) {
+                res.sendError(HttpServletResponse.SC_CONFLICT);
+                return;
+            }
+            out.print(objectMapper.writeValueAsString(c));
         }
     }
 }
--- WEB-INF/src/dao/CommandeDAO.java
+++ WEB-INF/src/dao/CommandeDAO.java
@@ -1,6 +1,7 @@
 package dao;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -70,5 +71,71 @@ public class CommandeDAO {
             ingredients.add(pizza);
         }
         return ingredients;
+    }
+
+    public Commande findById(int id){
+        con = ds.getConnection();
+        Commande commande = null;
+        try {
+            PreparedStatement ps = con.prepareStatement("SELECT * FROM commande where id = ?");
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                commande = new Commande(rs.getInt("id"), rs.getString("nom"), rs.getDate("dateCommande"));
+                commande.setPizzas(getPizzasFromCommande(commande.getId()));
+            }
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                con.close();
+            } catch (Exception e2) {
+            }
+
+        }
+        return commande;
+    }
+
+    public boolean save(Commande c){
+        con = ds.getConnection();
+        try {
+            PreparedStatement ps = con.prepareStatement("insert into commande (id, nom, dateCommande) values (?,?,?)");
+            ps.setInt(1, c.getId());
+            ps.setString(2, c.getNom());
+            ps.setDate(3, (Date) c.getDate());
+            int res = ps.executeUpdate();
+            if (res == 1) {
+                for (Pizzas pizza : c.getPizzas()) {
+                    PreparedStatement ps2 = con
+                            .prepareStatement("insert into pizzasCommande (idCom,idPiz) values (?,?)");
+                    ps2.setInt(1, c.getId()); 
+                    ps2.setInt(2, pizza.getId());
+                    ps2.executeUpdate();
+                    int res2 = ps2.executeUpdate();
+                    if(res2 == 1){
+                        for (Ingredients ingredient : pizza.getIngredients()) {
+                            PreparedStatement ps3 = con
+                                    .prepareStatement("insert into ingredientsPizza (idPiz,idIng) values (?,?)");
+                            ps3.setInt(1, pizza.getId());
+                            ps3.setInt(2, ingredient.getId());
+                            ps3.executeUpdate();
+                        }
+                        con.close();
+                        return true;
+                    }
+                }
+            }
+            
+        }catch (SQLException e) {
+            // TODO Auto-generated catch block
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                con.close();
+            } catch (Exception e2) {
+            }
+        }
+        return false;
     }
 }
